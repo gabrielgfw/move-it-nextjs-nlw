584bbc085f571ef1d3a0a6dfc63dc20e4ac30ca1
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2021-02-23 17:34:49 -0300

    add contador e botão do contador

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,24 @@
-import Head from 'next/head' 
 import { CompletedChallenges } from '../components/CompletedChallenges';
+import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from '../components/Profile';
+import Head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css'
 
 export default function Home() {
   return (
     <div className={ styles.container }>
+      <Head>
+        <title>Início | Move It</title>
+      </Head>
       <ExperienceBar/>
 
       <section>
         <div>
           <Profile />
           <CompletedChallenges/>
+          <Countdown />
         </div>
       </section>
   </div>
