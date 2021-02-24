315ee78e13e2d5122de711dceec03cee442f195c
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2021-02-24 17:34:02 -0300

    aplicando a context para os demais componentes e ajustando valores exibidos

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -10,10 +10,12 @@ interface Challenge {
 interface ChallengesContextData {
   level: number;
   currentExperience: number;
+  experienceToNextLevel: number;
   challengesCompleted: number;
   activeChallenge: Challenge;
   levelUp: () => void;
   startNewChallenge: () => void;
+  resetChallenge: () => void;
 }
 
 interface ChallengesProviderProps {
@@ -28,6 +30,8 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
   const [ challengesCompleted, setChallengesCompleted ] = useState(0);
   const [ activeChallenge, setActiveChallenge ] = useState(null);
   
+  const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
+
   function levelUp() {
     setLevel(level + 1);
   }
@@ -39,15 +43,21 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
     setActiveChallenge(challenge);
   }
 
+  function resetChallenge() {
+    setActiveChallenge(null);
+  }
+
   return (
     <ChallengesContext.Provider
       value={{ 
         level, 
-        currentExperience, 
+        currentExperience,
+        experienceToNextLevel, 
         challengesCompleted, 
         levelUp,
         startNewChallenge,
-        activeChallenge
+        activeChallenge,
+        resetChallenge
       }}
     >
       { children }
